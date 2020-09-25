81b215fd17f550f23b4ce318c99aa970276fea3e
Younes ben salem <youniben@icloud.com>
2020-09-25 20:32:14 +0200

    small fix

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ const App = () => (
         <Route exact path="/" component={Home} />
         <Route path="/planet/:name" component={DetailPage} />
         <Route path="/search/" component={SearchPage} />
+
         <Route component={NotFoundPage} />
       </Switch>
     </Page>
